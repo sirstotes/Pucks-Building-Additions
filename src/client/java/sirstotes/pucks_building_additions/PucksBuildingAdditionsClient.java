2c045d1662963df436ac2e585b86452c03e613fd
Isaiah Stotesbery <sirstotes@gmail.com>
2025-01-19 16:03:28 -0700

    Setup

---
+++ src/client/java/sirstotes/pucks_building_additions/PucksBuildingAdditionsClient.java
@@ -0,0 +1,10 @@
+package sirstotes.pucks_building_additions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PucksBuildingAdditionsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/sirstotes/pucks_building_additions/PucksBuildingAdditionsDataGenerator.java
@@ -0,0 +1,11 @@
+package sirstotes.pucks_building_additions;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PucksBuildingAdditionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
